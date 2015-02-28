430bd88791fb3c399050bb9c06374fb4eb42f1b7
bykapw <oleg@byka.pw>
2015-02-28 16:01:39 +0300

    ver 1.4

--- jquery.bpw-required.js
+++ jquery.bpw-required.js
@@ -1,4 +1,4 @@
-// version 1.3
+// version 1.4
 (function($) {
   $.fn.extend({
 	  require: function(options) {
@@ -8,15 +8,23 @@
         check=($(this).find('span.check'))[0];
       }
 
+      function check_item(ctl){
+        var val=$(ctl).val();
+        var re=$(ctl).attr('req-format') || '';
+        if((re != '' && val.match(re)) || (re == '' && val != '')){
+          $(ctl).addClass('req_ok').removeClass('req_error');
+        }else{
+          $(ctl).addClass('req_error').removeClass('req_ok');
+        }
+      };
+
 			var arr=$(this).find('.required');
 			for(var i=0;i<arr.length;i++){
         $(arr[i]).change(function(){
-          var val=$(this).val();
-          if(val == ''){
-            $(this).addClass('req_error').removeClass('req_ok');
-          }else{
-            $(this).addClass('req_ok').removeClass('req_error');
-          }
+          check_item(this);
+        });
+        $(arr[i]).keyup(function(){
+          check_item(this);
         });
       }
 
@@ -25,18 +33,13 @@
 			  var arr=$(this).find('.required:visible');
 			  var ret=true;
 			  for(var i=0;i<arr.length;i++){
-				  var val=$(arr[i]).val();
-				  if(val==''){
-					  $(arr[i]).addClass('req_error');
-					  ret=false;
-				  }else{
-					  $(arr[i]).removeClass('req_error');
-				  }
-				  if(!ret && check){
+          check_item(arr[i]);
+			  }
+			  var errs=$(this).find('.req_error');
+				if(errs.length > 0 && check){
             var str=options.text||'Some fields required!';
 					  $(check).text(str);
-				  }
-			  }
+				}
 			  return ret;
 		  });
 	  }
